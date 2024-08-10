abe8f8f38a53f7966d7177a966a9d40086fd93ee
Fariq Sutikno <fariq.sutikno@gmail.com>
2024-08-10 11:13:23 +0700

    Remove Seaborn from 01_Filter_Data_Alumni.py

--- pages/01_Filter_Data_Alumni.py
+++ pages/01_Filter_Data_Alumni.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from streamlit_dynamic_filters import DynamicFilters
 
 st.set_page_config(layout="wide")
